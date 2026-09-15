4e753728c799281fd779f99774be020939220baa
Max <kiporenkomaks8@gmail.com>
2026-09-15 17:08:14 +0300

    add book serializer

---
+++ book/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from book.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_free",
+        )
+
+
+class BookListSerializer(BookSerializer):
+    inventory = serializers.IntegerField(read_only=True)
+    daily_free = serializers.DecimalField(read_only=True, max_digits=6, decimal_places=2)
+
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_free",
+        )
+
+
+class BookDetailSerializer(BookSerializer):
+    pass
